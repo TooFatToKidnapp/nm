a3a8e0d3079289caa65ebdb988e85f3390d5e4be
TooFatToKidnapp <abdouali422@gmail.com>
2024-09-27 12:04:21 +0100

    added links to ELF file resources

--- includes/nm.h
+++ includes/nm.h
@@ -2,7 +2,9 @@
 #define NM_H
 
 #include <stdio.h>
-
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,8 +1,13 @@
 #include "../includes/nm.h"
 
-int main(int ac, char **av)
-{
+// ** the Executable and Linkable Format Resources **
+// https://www.cs.cmu.edu/afs/cs/academic/class/15213-f00/docs/elf.pdf
+// https://wiki.osdev.org/ELF
+// https://en.wikipedia.org/wiki/Executable_and_Linkable_Format
+// https://uclibc.org/docs/elf-64-gen.pdf
 
+int32_t main(int32_t ac, char **av)
+{
   (void)ac;
   (void)av;
   printf("Hello World\n");
